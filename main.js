eb70ff3e3cf6fcd67fb5c95ebabe688908b5e089
mclausen792 <mclausen792@gmail.com>
2016-11-20 10:16:27 -0700

    jquery-1

---
+++ main.js
@@ -0,0 +1,110 @@
+/*
+=======================================================
+
+ ** Week 4 - Project 1  **
+*** Exercise the jQuery ***
+
+We'll be using the html code within the index.html for the
+following jQuery exercises. After you've completed an exercise,
+comment out your code (unless otherwise indicated) as these
+techniques may conflict with each other.
+
+=======================================================
+*/
+
+// Add your JS here.
+
+
+
+
+
+// 1. Make sure that the HTML document is fully loaded before running any jQuery code that is needed below. Make sure you don't comment out this code as you'll wrap this code around all the following methods (so don't forget the closing tags, too.
+
+$(document).ready(function() {
+
+  // console.log("hello");
+
+
+
+
+
+// 2. Hide all of the paragraphs on the index.html page.
+// $('p').hide()
+// });
+
+
+// Hides all paragraphs
+
+
+
+// 3. Hide all of the h2's on the page.
+// $("h2").hide()
+// });
+
+// Hides all h2's
+
+
+
+
+// 4. Hide all images on the page.
+// $("img").hide()
+// });
+
+// Hides all images
+
+
+
+
+// 5. Hide all links on the page.
+// $('a').hide()
+// });
+
+// Hides all links
+
+
+// 6. Hide all of the elements in the document.
+// $("body").hide()
+// });
+
+// Hides all elements
+
+
+
+// 7. Hide all paragraphs when one of them is clicked.
+// $("p").click(function(){
+//   $("p").hide()
+// })
+// });
+
+// Hides all paragraphs upon clicking :)
+
+
+// 8. Hide all images when one of them is clicked.
+// $("img").click(function(){
+//   $("img").hide()
+// })
+// });
+
+// Hides all clicked images
+
+// 9. When the user clicks onto the image, toggle the paragraphs between hide and show.
+// $("img").click(function(){
+//   $("p").toggle()
+// })
+// });
+
+// paragraphs hide and reappear when image is clicked
+
+
+/*
+SANDBOX:
+Based on the code that is used to hide all images when clicked, consider testing the following options to test the interaction.
+
+Interactive Methods: dblclick(), hover(), mousedown(), mouseenter(), mouseleave(), mousemove(), mouseout(), mouseover(), and mouseup().
+
+
+Update previous hide functions with speed.
+
+How does the speed affect the animated techniques as they are applied to different HTML elements?
+
+*/
